33b9ebd523f8d5f7e747dab8cad377d02a04adf9
Dominique Makowski <dom.mak19@gmail.com>
2023-09-13 14:14:22 +0100

    Update stream.py

--- MuseBSL/stream.py
+++ MuseBSL/stream.py
@@ -2,7 +2,6 @@ import bsl
 
 
 def stream(address):
-    print("A")
     info_eeg = bsl.lsl.StreamInfo(
         name="Muse",
         stype="EEG",
@@ -11,17 +10,16 @@ def stream(address):
         dtype="float32",
         source_id=address,
     )
-    print("B")
-    # Add additional information
 
-    info_eeg.desc().append_child_value("manufacturer", "Muse")
-    eeg_channels = info_eeg.desc().append_child("channels")
+    # # Add additional information
+    # info_eeg.desc().append_child_value("manufacturer", "Muse")
+    # eeg_channels = info_eeg.desc().append_child("channels")
 
-    for channel in ["TP9", "AF7", "AF8", "TP10", "Right AUX"]:
-        eeg_channels.append_child("channel").append_child_value(
-            "label", channel
-        ).append_child_value("unit", "microvolts").append_child_value("type", "EEG")
+    # for channel in ["TP9", "AF7", "AF8", "TP10", "Right AUX"]:
+    #     eeg_channels.append_child("channel").append_child_value(
+    #         "label", channel
+    #     ).append_child_value("unit", "microvolts").append_child_value("type", "EEG")
 
     print(info_eeg.desc())
-    eeg_outlet = bsl.lsl.StreamOutlet(info_eeg, 12)
+    eeg_outlet = bsl.lsl.StreamOutlet(info_eeg, chunk_size=6, max_buffered=360)
     print(eeg_outlet)

896cf21082ff3adc382d19344486e07214c9634a
Dominique Makowski <dom.mak19@gmail.com>
2023-09-13 14:17:48 +0100

    Update stream.py

--- MuseBSL/stream.py
+++ MuseBSL/stream.py
@@ -11,14 +11,14 @@ def stream(address):
         source_id=address,
     )
 
-    # # Add additional information
+    # Add additional information
     info_eeg.desc.append_child_value("manufacturer", "Muse")
-    # eeg_channels = info_eeg.desc().append_child("channels")
+    eeg_channels = info_eeg.desc.append_child("channels")
 
-    # for channel in ["TP9", "AF7", "AF8", "TP10", "Right AUX"]:
-    #     eeg_channels.append_child("channel").append_child_value(
-    #         "label", channel
-    #     ).append_child_value("unit", "microvolts").append_child_value("type", "EEG")
+    for channel in ["TP9", "AF7", "AF8", "TP10", "Right AUX"]:
+        eeg_channels.append_child("channel").append_child_value(
+            "label", channel
+        ).append_child_value("unit", "microvolts").append_child_value("type", "EEG")
 
     print(info_eeg.desc)
     eeg_outlet = bsl.lsl.StreamOutlet(info_eeg, chunk_size=6, max_buffered=360)

ae8002a895cb7b4a355bf43a7f078e3091183314
Dominique Makowski <dom.mak19@gmail.com>
2023-09-13 14:45:54 +0100

    tidy

--- MuseBSL/muse.py
+++ MuseBSL/muse.py
@@ -5,22 +5,35 @@ class Muse:
     """Muse EEG headband"""
 
     def __init__(self, address=None):
-        self.address = address
+        # Create Bluetooth adapter
         self.adapter = BleakBackend()
 
-    def find(self, max_duration=10):
+        # Assign MAC address
+        self.address = address
+        if address is None:
+            muses = self.find(verbose=False)
+            if len(muses) == 0:
+                print("No Muses found, make sure it's connected.")
+            if len(muses) == 1:
+                self.address = muses[0]["address"]
+            if len(muses) > 1:
+                print("Multiple Muses found. Please specify MAC address.")
+                self.address = muses[0]["address"]  # Pick first one
+
+    def find(self, max_duration=10, verbose=False):
         self.adapter.start()
         print(f"Searching for Muses (max. {max_duration} seconds)...")
         devices = self.adapter.scan(timeout=10.5)  # Muse scan timeout
         self.adapter.stop()
         muses = [d for d in devices if d["name"] and "Muse" in d["name"]]
 
-        for m in muses:
-            print(f'Found device {m["name"]}, MAC Address {m["address"]}')
-        if not muses:
-            print("No Muses found, make sure it's connected.")
+        if verbose:
+            for m in muses:
+                print(f'Found device {m["name"]}, MAC Address {m["address"]}')
+            if not muses:
+                print("No Muses found, make sure it's connected.")
 
-        return muses[0]
+        return muses
 
     def connect(self):
         self.adapter.start()

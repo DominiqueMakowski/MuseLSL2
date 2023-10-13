1198dcd03473b1addaeaff67e5dfe80cb0d8ed56
Dominique Makowski <dom.mak19@gmail.com>
2023-10-13 13:59:06 +0100

    mne-lsl is dep

--- setup.py
+++ setup.py
@@ -1,20 +1,20 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="MuseBSL",
-    version="0.1",
-    description="Python package for streaming, recording, and visualizing data from the Muse EEG headset using the Brain Streaming Layer (BSL).",
+    name="MuseLSL2",
+    version="0.2",
+    description="Python package for streaming, recording, and visualizing data from the Muse EEG headset using mne-lsl (an updated implementation of LSL).",
     author="Dominique Makowski",
     author_email="D.Makowski@sussex.ac.uk",
     license="MIT",
-    url="https://github.com/DominiqueMakowski/MuseBSL",
-    entry_points={"console_scripts": ["MuseBSL=MuseBSL.__main__:main"]},
+    url="https://github.com/DominiqueMakowski/MuseLSL2",
+    entry_points={"console_scripts": ["MuseLSL2=MuseLSL2.__main__:main"]},
     install_requires=[
         "numpy",
         "matplotlib",
+        "https://github.com/mne-tools/mne-lsl/zipball/main",  # Replaces lsl
         "bleak",  # Bluetooth backend
         "bitstring",  # For decoding Muse packets
-        "bsl >= 0.6.4",  # Brain Streaming Layer (replaces lsl)
         "vispy",  # Visualization
     ],
     packages=find_packages(),

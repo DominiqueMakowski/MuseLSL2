4011f5434e3031e25171ad73dadfa3e270abda21
Dominique Makowski <dom.mak19@gmail.com>
2023-10-24 14:48:23 +0100

    rename right aux

--- load_data.py
+++ load_data.py
@@ -2,8 +2,10 @@ import neurokit2 as nk
 
 # Load data
 data, info = nk.read_xdf(
-    "data/sub-P001/ses-S001/eeg/sub-P001_ses-S001_task-Default_run-001_eeg.xdf"
+    "data/sub-P002/ses-S001/eeg/sub-P002_ses-S001_task-Default_run-001_eeg.xdf"
 )
 
 data = data.reset_index(drop=True)
 data.plot(subplots=True)
+
+data["Right AUX"]
